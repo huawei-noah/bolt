5944fbae971b25b53ec3d546f904b3c0838496a6
Xianzhi Yu <cos_wave@163.com>
2021-10-17 18:28:46 +0800

    Update topk.cpp

--- compute/tensor/src/cpu/topk.cpp
+++ compute/tensor/src/cpu/topk.cpp
@@ -11,8 +11,8 @@
 // COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-#include <algorithm>
 #include "cpu/tensor_computing_cpu.h"
+#include <algorithm>
 
 template <typename T, bool increase, bool order>
 inline static void topk_kernel(
